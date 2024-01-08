6c5ec245363b5d2eeeb256018e34319ed36c76c6
Tarik <hajibtarik@gmail.com>
2024-01-08 21:16:31 +0100

    Animations

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import AnimatedHero from "../ui/Animations/AnimatedHero";
 
 function Hero() {
   return (
     <section>
-      <div className="p-8 lg:p-16 grid h-screen grid-rows-12 absolute z-10 text-white">
-        <h1 className="row-start-2 lg:row-start-2 z-50 scroll-m-20 text-5xl font-medium tracking-tight md:text-7xl lg:text-8xl">
-          Opplev storhet
-          <br /> i mikrohus.
-        </h1>
+      <div className="p-8 lg:p-16 grid max-w-screen h-screen grid-rows-12 absolute z-10 text-white">
+        <AnimatedHero />
         <h2 className="scroll-m-20 text-4xl tracking-tight row-start-11 lg:row-start-12">
           Smart, bærekraftig mikrohus. Levert.
         </h2>
---
+++ components/ui/Animations/AnimatedHero.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { motion } from "framer-motion";
+
+const heading = "Opplev Storhet";
+const heading2 = "i mikrohus";
+
+const letters = Array.from(heading);
+const letters2 = Array.from(heading2);
+
+const letterAnimation = {
+  initial: {
+    y: 200,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 0.6,
+    },
+  },
+};
+
+const banner = {
+  animate: {
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const AnimatedLetters = ({ title }: { title: string }) => {
+  const letters = Array.from(title);
+  return (
+    <motion.span
+      className="flex overflow-hidden max-w-screen-sm"
+      variants={banner}
+      initial="initial"
+      animate="animate"
+    >
+      {letters.map((letter) => (
+        <motion.span variants={letterAnimation} key={letter}>
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.span>
+  );
+};
+
+const AnimatedHero = () => {
+  return (
+    <motion.div className="flex flex-col row-start-2 row-span-auto lg:row-start-2 z-50 scroll-m-20 text-5xl font-medium tracking-tight md:text-7xl lg:text-8xl h-fit">
+      <AnimatedLetters title="Opplev storhet" />
+      <AnimatedLetters title="i mikrohus" />
+    </motion.div>
+  );
+};
+
+export default AnimatedHero;
