d2b9db9f362d9cf6c3e151c51cd4ac287cb987a8
Emil <emsor2002@outlook.com>
2024-01-16 18:35:18 +0100

    Updated FAQ

--- components/sections/FAQ.tsx
+++ components/sections/FAQ.tsx
@@ -9,7 +9,7 @@ import { AnimatedLetters, AnimatedLines } from "../ui/Animations/AnimatedHero";
 
 export function FAQ() {
   return (
-    <Accordion type="single" collapsible className="my-16 p-3 lg:p-4">
+    <Accordion type="single" collapsible className="my-16 p-3 lg:p-4 w-full">
       <h2 className="text-[clamp(32px,9.6vw,36px)] md:text-[clamp(42px,3.3vw,48px)] tracking-tighter my-8">
         <AnimatedLetters text="Ofte stilte spørsmål" />
       </h2>
