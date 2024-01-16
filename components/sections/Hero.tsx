bf4b9f8494d361a9e493de2c711a5153fe2879fe
Emil <emsor2002@outlook.com>
2024-01-16 22:27:04 +0100

    fixed arrow in hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -13,15 +13,20 @@ function Hero() {
         <h2 className="scroll-m-20 text-4xl tracking-tight row-start-11 lg:row-start-12">
           <AnimatedLetters text="Smart, bærekraftig mikrohus. Levert." />
         </h2>
-        <AnimateBounce>
-          <Image
-            className="row-start-11 lg:row-start-12 rotate-90 justify-self-end"
-            alt="Mikrohus"
-            src={"/arrowWhite.svg"}
-            width={100}
-            height={100}
-          />
-        </AnimateBounce>
+
+        <div className="relative bg-red-200 row-start-11 lg:row-start-12 justify-self-end">
+          <div className="absolute md:h-[100px] md:w-[100px] bottom-0 right-0">
+            <AnimateBounce>
+              <Image
+                className="rotate-90"
+                alt="Mikrohus"
+                src={"/arrowWhite.svg"}
+                width={100}
+                height={100}
+              />
+            </AnimateBounce>
+          </div>
+        </div>
       </div>
       <div className="h-screen relative overflow-hidden">
         <Image
