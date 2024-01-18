98db8e33419d81ba0feee12efbe839adb95326c9
Emil <emsor2002@outlook.com>
2024-01-18 22:58:59 +0100

    h fix

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -11,7 +11,7 @@ function Hero() {
       <div
         data-scroll
         data-scroll-speed="-.1"
-        className="p-8 lg:p-16 grid h-svh h-screen grid-rows-12 absolute z-10 text-white w-full"
+        className="p-8 lg:p-16 grid h-svh sm:h-screen grid-rows-12 absolute z-10 text-white w-full"
       >
         <AnimatedHero />
         <h2 className="col-span-full scroll-m-20 text-4xl tracking-tight row-start-11 lg:row-start-12">
