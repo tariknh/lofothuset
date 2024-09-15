5d3840d7541842c06773891ee4581f08f7d33f1f
Tarik <hajibtarik@gmail.com>
2024-09-15 20:16:29 +0200

    Removed FAQ temporarily from the other branch too

--- app/(client)/modeller/[modeller]/page.tsx
+++ app/(client)/modeller/[modeller]/page.tsx
@@ -1,14 +1,8 @@
 import ModellBestille from "@/components/modeller/ModellBestille";
-import ModellFordeler from "@/components/modeller/ModellFordeler";
 import ModellFordelerV2 from "@/components/modeller/ModellFordelerV2";
 import ModellHero from "@/components/modeller/ModellHero";
 import ModellModeller from "@/components/modeller/ModellModeller";
-import ModellVideo from "@/components/modeller/ModellVideo";
-import Blog from "@/components/sections/Blog";
-import { FAQ } from "@/components/sections/FAQ";
 import Hvordan from "@/components/sections/Hvordan";
-import Transition from "@/components/ui/Animations/Transition";
-import React from "react";
 import modellerList from "@/public/Static/Modeller.json";
 
 function page({ params }: { params: { modeller: string } }) {
@@ -40,7 +34,7 @@ function page({ params }: { params: { modeller: string } }) {
 
       <Hvordan />
 
-      <FAQ />
+      {/* <FAQ /> Issue with loading page */}
     </main>
   );
 }
